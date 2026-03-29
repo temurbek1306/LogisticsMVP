25613aa2a1cf83e810089ef2a4e457ab39e6392f
Temurbek <temurbekgulboyev01@gmail.com>
2026-03-29 11:36:38 +0500

    Fix Prisma build error: Added return type and fixed naming in route handler

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,7 @@ declare global {
   var prisma: undefined | PrismaClient
 }
 
-export const getPrisma = () => {
+export const getPrisma = (): PrismaClient => {
   const prisma = globalThis.prisma ?? new PrismaClient()
   if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
   return prisma
